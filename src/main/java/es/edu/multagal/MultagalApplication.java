b5c38941ae5233ae3fef7b75bae15012157aaa57
perfectolorenzo <lorenzocouceirop@gmail.com>
2026-09-16 20:34:04 +0200

    first commit

---
+++ src/main/java/es/edu/multagal/MultagalApplication.java
@@ -0,0 +1,13 @@
+package es.edu.multagal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultagalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultagalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/edu/multagal/MultagalApplicationTests.java
@@ -0,0 +1,13 @@
+package es.edu.multagal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultagalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
